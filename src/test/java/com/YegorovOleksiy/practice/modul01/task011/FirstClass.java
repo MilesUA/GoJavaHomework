e3390a33d4c627264cf108f89b4e1fcda7b4d9bd
milesua <milesua@ukr.net>
2017-02-03 20:25:21 +0200

    ДЗ

---
+++ src/test/java/com/YegorovOleksiy/practice/modul01/task011/FirstClass.java
@@ -0,0 +1,10 @@
+package com.YegorovOleksiy.practice.modul01.task011;
+
+/**
+ * Created by Miles on 03.02.2017.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мій перший клас на Java");
+    }
+}
--- src/test/java/com/YegorovOleksiy/practice/modul01/task011/SecondClass.java
+++ src/test/java/com/YegorovOleksiy/practice/modul01/task011/SecondClass.java
@@ -3,7 +3,7 @@ package com.YegorovOleksiy.practice.modul01.task011;
 /**
  * Created by Miles on 03.02.2017.
  */
-public class FirstProgramm {
+public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Мій перший клас на Java");
     }
