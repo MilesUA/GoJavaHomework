a5e637f889606389da67f1651b5cda3feab2cbdb
milesua <milesua@ukr.net>
2017-02-12 19:42:18 +0200

    ДЗ 2 виправлено

--- src/main/java/com/YegorovOleksiy/practice/modul02/task021/sum.java
+++ src/main/java/com/YegorovOleksiy/practice/modul02/task021/sum.java
@@ -3,22 +3,43 @@ package com.YegorovOleksiy.practice.modul02.task021;
 /**
  * Created by Miles on 06.02.2017.
  */
-public class sum {
+public class Sum {
     public static void main(String[] args) {
-        int[] array = new int[10];
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            array[i] = getRandomInteger(100);
-            sum += array[i];
-            System.out.println("Number " + (i + 1) + " = " + array[i]);
+
+        int[] intArray = {1, 2, 3, 10, -5};
+        double[] doubleArray = {1, 2, 3, 10, -5};
+
+        Sum mySum = new Sum();
+        
+        mySum.methodSum(intArray);
+        mySum.methodSum(doubleArray);
+
+    }
+
+    public void methodSum(double[] doubleArray) {
+
+        double doubleSum = 0;
+        for (int i = 0; i < doubleArray.length; i++) {
+
+            doubleSum += doubleArray[i];
+            System.out.println("Number " + (i + 1) + " = " + doubleArray[i]);
         }
-        System.out.println("sum = " + sum);
+
+        System.out.println("DoubleSum = " + doubleSum);
+
     }
 
-    private static int getRandomInteger(int maxValue) {
+    public void methodSum(int[] intArray) {
 
-        int randomValue = (int) (Math.random() * maxValue);
+        int intSum = 0;
+        for (int i = 0; i < intArray.length; i++) {
 
-        return randomValue;
+            intSum += intArray[i];
+            System.out.println("Number " + (i + 1) + " = " + intArray[i]);
+        }
+
+        System.out.println("IntSum = " + intSum);
+
     }
+
 }
