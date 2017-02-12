52bd12b5afe317b736b3a7bfc246ede492230038
milesua <milesua@ukr.net>
2017-02-12 21:02:31 +0200

    ДЗ 2 виправлено

--- src/main/java/com/YegorovOleksiy/practice/modul02/task024/fundBalance.java
+++ src/main/java/com/YegorovOleksiy/practice/modul02/task024/fundBalance.java
@@ -3,15 +3,15 @@ package com.YegorovOleksiy.practice.modul02.task024;
 /**
  * Created by Miles on 06.02.2017.
  */
-public class fundBalance {
+public class FundBalance {
     public static void main(String[] args) {
 
         int[] balances = {1200, 250, 2000, 500, 3200};
         String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
 
         int accountOwner = -1;
-        double withdrawal = 100;
-        String ownerName = "Oww";
+        int withdrawal = 100;
+        String ownerName = "Ann";
 
         for (int i = 0; i < ownerNames.length; i++) {
             if (ownerName == ownerNames[i])
@@ -19,18 +19,19 @@ public class fundBalance {
                 accountOwner = i;
                 break;
             }
+
         }
 
-        int balance = balances[accountOwner];
-        double balanceAfter = 0;
+        FundBalance myFundBalance = new FundBalance();
+        myFundBalance.methodFundBalance(balances[accountOwner], ownerNames[accountOwner], withdrawal);
 
-        balanceAfter = getBalanceAfter(balance, withdrawal);
-
-        System.out.println(ownerNames[accountOwner] + " " + balanceAfter);
     }
 
-    private static int getBalanceAfter(int balance, double withdrawal) {
-        double balanceAfter = balance + withdrawal;
-        return (int) balanceAfter;
+    private void methodFundBalance(int balance, String ownerName, int withdrawal) {
+
+        int balanceAfter = balance + withdrawal;
+        System.out.println(ownerName + " " + balanceAfter);
     }
+
+
 }
