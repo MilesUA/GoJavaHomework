6665348c8ac36ab4a82d29d628251216576340cd
milesua <milesua@ukr.net>
2017-03-06 00:05:56 +0200

    ДЗ 3.2

--- src/main/java/com/YegorovOleksiy/practice/modul03/task02/Adder.java
+++ src/main/java/com/YegorovOleksiy/practice/modul03/task02/Adder.java
@@ -6,8 +6,11 @@ package com.YegorovOleksiy.practice.modul03.task02;
 public class Adder extends Arithmetic {
 
 
+    public Adder(int a, int b) {
+        super(a, b);
+    }
 
-    public static boolean check() {
+    public static boolean check(int a, int b) {
 
         if (a >= b){
             return (true);
--- src/main/java/com/YegorovOleksiy/practice/modul03/task02/Arithmetic.java
+++ src/main/java/com/YegorovOleksiy/practice/modul03/task02/Arithmetic.java
@@ -4,11 +4,30 @@ package com.YegorovOleksiy.practice.modul03.task02;
  * Created by Miles on 05.03.2017.
  */
 public class Arithmetic {
-    static final int a = 8;
-    static final int b = 5;
+
+    private int a;
+    private int b;
 
 
-    public static int add(){
+   public static void main(String[] args) {
+
+       Arithmetic arithmetic = new Arithmetic();
+       Adder adder = new Adder(arithmetic.a, arithmetic.b);
+
+        System.out.println("Sum = " + arithmetic.add(8, 5));
+        System.out.println("a > b ? It is " + adder.check(arithmetic.a, arithmetic.b) );
+
+    }
+
+    public Arithmetic() {
+    }
+
+    public Arithmetic(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public static int add(int a, int b){
         int sum = a + b;
 
         return sum;
